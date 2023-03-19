447e2e2183f8b80ef07de313507f2b8727e1e4b3
Akhil Kurup <akhil.kurup@inseinc.com>
2023-03-19 11:30:52 +0530

    spring cloud

--- learn-spring-cloud/microservice-1/src/main/java/com/learnspringcloud/microservice/feignclients/PaymentFeignClient.java
+++ learn-spring-cloud/microservice-1/src/main/java/com/learnspringcloud/microservice/feignclients/PaymentFeignClient.java
@@ -3,7 +3,7 @@ package com.learnspringcloud.microservice.feignclients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "api-gateway")
+@FeignClient(value = "api-gateway")
 public interface PaymentFeignClient {
 
 	@GetMapping("/micro-service2/get-payment-id")
